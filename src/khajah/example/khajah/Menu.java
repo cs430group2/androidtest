e552ae5e47693f685b3d099d8a85354a797ad966
abbas khajah <abbaskhajah@gmail.com>
2012-10-01 00:18:07 -0700

    update code

--- src/khajah/example/khajah/Menu.java
+++ src/khajah/example/khajah/Menu.java
@@ -1,14 +1,17 @@
 package khajah.example.khajah;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Menu extends ListActivity{
 	
 	
-	String classes[] = { "MainActivity", "example1", "example2", "example3", "example4", "example5", "example6"};
+	String classes[] = { "MainActivity", "example1", "example2", "example3", 
+			"example4", "example5", "example6"};
 	
 	//classes[4];
 	
@@ -16,14 +19,28 @@ public class Menu extends ListActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter <String>(Menu.this, android.R.layout.simple_list_item_1, classes) ); // create array adapter and String Type 
+	
 	}
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		// TODO Auto-generated method stub
+		
+		
 		super.onListItemClick(l, v, position, id);
+		String cheese = classes[position]; // choose what position you need  
+		try{
+		@SuppressWarnings("rawtypes")
+		Class ourClass = Class.forName("khajah.example.khajah.MainActivity." + cheese);
+		Intent ourIntent = new Intent(Menu.this, ourClass);
+		startActivity(ourIntent);
+	
+	}catch(ClassNotFoundException e){ // for debugger 
+		e.printStackTrace();
 	}
 
 	
+	}
 
 }
